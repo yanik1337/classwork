7f101572c058bc530e1e3c465318bb532f525bc2
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-23 18:37:29 +0300

    5zadanie

---
+++ ReverseListClass.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassWork3.ReverseListClass
+{
+    public class ReverseList
+    {
+        public static void Reverse(List<int> list)
+        {
+            if (list == null || list.Count <= 1)
+            {
+                return;
+            }
+
+            int left = 0;
+            int right = list.Count - 1;
+
+            while (left < right)
+            {
+                int temp = list[left];
+                list[left] = list[right];
+                list[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
