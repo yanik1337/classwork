1f6dcfe76634479c957dbb4b3c7e99ee69d9b448
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-23 18:36:32 +0300

    2zadanie
    ewrqewstgfasdgf

---
+++ UniqueCounterClass.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassWork3.UniqueCounterClass
+{
+    public class UniqueCounter
+    {
+        public static int CountUnique(List<int> numbers)
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (i != j && numbers[i] == numbers[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
