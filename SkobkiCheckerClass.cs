c1f2aa5621848323fbde513e4197391615af9cd6
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-23 18:36:55 +0300

    4zadanie
    sdfads

---
+++ SkobkiCheckerClass.cs
@@ -0,0 +1,39 @@
+﻿namespace ClassWork3.SkobkiCheckerClass
+{
+    public class SkobkiChecker
+    {
+        public static bool IsBalanced(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            Stack<char> bracketStack = new Stack<char>();
+
+            Dictionary<char, char> bracketPairs = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+
+            foreach (char c in input)
+            {
+                if (bracketPairs.ContainsKey(c))
+                {
+                    bracketStack.Push(c);
+                }
+                else if (bracketPairs.ContainsValue(c))
+                {
+                    if (bracketStack.Count == 0 || bracketPairs[bracketStack.Pop()] != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return bracketStack.Count == 0;
+        }
+    }
+}
