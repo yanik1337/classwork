4908465fbb8e779d9c9195211a683b6e4645b7ec
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-23 18:36:10 +0300

    1zadanie
    fasfasdfewfaewdfs

---
+++ ElementFinderClass.cs
@@ -0,0 +1,32 @@
+﻿namespace ClassWork3.ElementFinderClass
+{
+    public class ElementFinder
+    {
+        public static int[] Find(int[] array)
+        {
+            int[] result = new int[array.Length];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0 && array[stack.Peek()] <= array[i])
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count > 0)
+                {
+                    result[i] = array[stack.Peek()];
+                }
+                else
+                {
+                    result[i] = 1;
+                }
+
+                stack.Push(i);
+            }
+
+            return result;
+        }
+    }
+}
